850ec826fe8a2f15b823b6a48c366f13ff87526c
yaikohi <erik.beem@hotmail.com>
2023-06-19 18:13:50 +0200

    styling update

--- src/components/poe-creator/poe-creator.tsx
+++ src/components/poe-creator/poe-creator.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import {
   LuAlbum,
   LuCalendar,
@@ -7,22 +7,25 @@ import {
 } from "@qwikest/icons/lucide";
 
 export const PoeCreator = component$(() => {
+  const poeContent = useSignal<string>("");
+
   return (
-    <div class="flex w-full border-border border-[1px] gap-2 p-4">
+    <div class="flex w-full  border-b-border border-b-[1px] gap-2 p-4">
       <div class="aspect-square h-10 bg-green-200 rounded-full"></div>
-      <div class="flex w-full flex-col">
-        <div class="bg-muted">
+      <div class="flex w-full flex-col ">
+        <div class="u">
           <label hidden={true} class="text-xs" for="poe">
             poe
           </label>
           <textarea
             placeholder="What is happening?"
-            class="p-2 flex min-h-[80px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-lg ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            class="p-2 flex min-h-[80px] w-full resize-none rounded-md  bg-transparent px-3 py-2 text-lg ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
             name="poe"
+            bind:value={poeContent}
           />
         </div>
-        <div class="flex justify-between border-border pt-2 border-t-[1px] my-2">
-          <div class="flex gap-4 ">
+        <div class="flex justify-between pt-2 my-2">
+          <div class="flex gap-4 invisible">
             <button class="">
               <LuCalendar class="w-5 h-5" />
             </button>
@@ -36,7 +39,12 @@ export const PoeCreator = component$(() => {
               <LuSmile class="w-5 h-5" />
             </button>
           </div>
-          <button class="px-3 py-2 rounded-xl bg-slate-50 border-border border-[1px] my-1">
+          <button
+            onClick$={() => {
+              console.log(poeContent.value);
+            }}
+            class="px-3 py-2 rounded-xl bg-primary text-primary-foreground my-1"
+          >
             poet
           </button>
         </div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { LuBarChart2, LuHeart, LuRepeat, LuReply } from "@qwikest/icons/lucide";
 import { PoeCreator } from "~/components/poe-creator/poe-creator";
 
 function getFormattedDate(date: Date) {
@@ -8,7 +9,7 @@ function getFormattedDate(date: Date) {
     dateStyle: "full",
   }).format(date);
 }
-
+/** WIP relative time calculator function lol */
 // function getRelativeTime(date: Date) {
 //   return new Intl.RelativeTimeFormat("en", {
 //     numeric: "auto",
@@ -26,11 +27,11 @@ export default component$(() => {
   };
   return (
     <>
-      <div class="flex  justify-between flex-row">
+      <div class="flex justify-between flex-row">
         <div class="flex flex-row-reverse max-w-[200px] bg-blue-50 w-full">
-          sidebar1
+          {/* sidebar1 */}
         </div>
-        <main class="max-w-lg w-full">
+        <main class="max-w-lg border-border border-[1px]  w-full">
           {/* TABS */}
           <div class="flex w-full place-content-around">
             <div class="px-3 py-2 p-2 m-1">
@@ -50,23 +51,9 @@ export default component$(() => {
             <Poe {...obj} />
             <Poe {...obj} />
             <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
-            <Poe {...obj} />
           </div>
         </main>
-        <div class="flex bg-blue-50 max-w-[200px] w-full">sidebar2</div>
+        <div class="flex bg-blue-50 max-w-[200px] w-full">{/* sidebar2 */}</div>
       </div>
     </>
   );
@@ -83,15 +70,16 @@ interface PoeProps {
 export const Poe = component$<PoeProps>(({ date, user, content }) => {
   const { name, username } = user;
   return (
-    <div class="my-4 mx-2">
-      <div class="flex flex-row gap-2">
+    <div class="pt-2 w-full border-b-border border-b-[1px]">
+      <div class="mx-2 flex flex-row gap-2">
         <div class="aspect-square h-10 rounded-full bg-green-200"></div>
         <div class="flex flex-col">
           <div class="flex place-items-center justify-between">
             <div class="flex gap-1">
               <h3 class="font-bold">{name}</h3>
-              <p class="font-light text-gray-600">@{username}</p>
+              <p class="font-light text-muted-foreground">@{username}</p>
             </div>
+            {/* Should be relative to todays date. */}
             <time
               class="text-muted-foreground text-xs"
               dateTime={new Date().toISOString()}
@@ -100,6 +88,20 @@ export const Poe = component$<PoeProps>(({ date, user, content }) => {
             </time>
           </div>
           <p>{content}</p>
+          <div class="flex gap-20 my-2">
+            <button>
+              <LuReply />
+            </button>
+            <button>
+              <LuRepeat />
+            </button>
+            <button>
+              <LuBarChart2 />
+            </button>
+            <button>
+              <LuHeart />
+            </button>
+          </div>
         </div>
       </div>
     </div>
